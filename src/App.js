05799eceee5c18fa3279532dd63eedeef2f28bc6
Mychal Hall <hallmychal@gmail.com>
2019-03-07 20:29:01 -0600

    Well I built it in all the files... refactoring

--- src/App.js
+++ src/App.js
@@ -2,7 +2,34 @@ import React from "react";
 import TodoList from "./components/TodoComponents/TodoList";
 import "./components/TodoComponents/Todo.css";
 
+const todos = [
+  {
+    task: "Testomo",
+    id: Date.now(),
+    completed: false
+  },
+  {
+    task: "Testo2",
+    id: Date.now(),
+    completed: false
+  },
+  {
+    task: "Testo3",
+    id: Date.now(),
+    completed: false
+  }
+];
+
 class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      stateTodo: todos,
+      task: "",
+      id: Date.now(),
+      completed: false
+    };
+  }
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
